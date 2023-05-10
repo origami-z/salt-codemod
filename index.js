66aec51d6794674b7a16682202e305ab05ff69a4
Zhihao Cui <davidcui2@me.com>
2023-05-10 16:48:47 +0100

    - adds core 1.4.0 migration (#6)
    - initial attempt to support monorepo migration using ts-morph source api

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import { hideBin } from "yargs/helpers";
 import glob from "fast-glob";
 import { join } from "path";
 import process from "process";
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync, existsSync } from "fs";
 import { parse, lte, lt, gt, gte, SemVer } from "semver";
+
+import {
+  css100RenameMap,
+  css130RenameMap,
+  css140RenameMap,
+} from "./cssMaps.js";
 
 const yargs = _yargs(hideBin(process.argv));
 
@@ -13,11 +19,13 @@ const v100 = parse("1.0.0");
 const v110 = parse("1.1.0");
 const v120 = parse("1.2.0");
 const v130 = parse("1.3.0");
+const v140 = parse("1.4.0");
 
-const latestSupportedVersion = "1.3.0";
+const latestSupportedVersion = "1.4.0";
 
 var {
   tsconfig,
+  tsSourceGlob,
   verbose,
   organizeImports,
   dryRun,
@@ -32,6 +40,11 @@ var {
     default: "tsconfig.json",
     description:
       "React code is modified using ts-morph, which needs a path to your tsconfig.json file.",
+  })
+  .option("tsSourceGlob", {
+    default: "packages/**/*.ts*",
+    description:
+      "React source glob to be used by ts-morph, which is useful for monorepo where tsconfig is not available at the root.",
   })
   .option("verbose", {
     type: "boolean",
@@ -78,106 +91,33 @@ console.log(
   toVersion.format()
 );
 
-// Everything here is salt prefixed, assuming uitk prefix rename is performed first
-const css100RenameMap = [
-  ["--salt-container-background", "--salt-container-primary-background"],
-  [
-    "--salt-container-background-medium",
-    "--salt-container-secondary-background",
-  ],
-  ["--salt-container-background-low", "--salt-container-tertiary-background"],
-
-  ["--salt-editable-background", "--salt-editable-primary-background"],
-  ["--salt-editable-background-low", "--salt-editable-primary-background"],
-  ["--salt-editable-background-medium", "--salt-editable-secondary-background"],
-  ["--salt-editable-background-high", "--salt-editable-tertiary-background"],
-  ["--salt-editable-text-color", "--salt-text-primary-foreground"],
-
-  [
-    "--salt-selectable-background-selected",
-    "--salt-selectable-cta-background-selected",
-  ],
-  [
-    "--salt-selectable-foreground-selected",
-    "--salt-selectable-cta-foreground-selected",
-  ],
-
-  ["--salt-separable-border-color-1", "--salt-separable-tertiary-borderColor"],
-
-  [
-    "--salt-status-info-background-high",
-    "--salt-status-info-background-emphasize",
-  ],
-  [
-    "--salt-status-success-background-high",
-    "--salt-status-success-background-emphasize",
-  ],
-  [
-    "--salt-status-warning-background-high",
-    "--salt-status-warning-background-emphasize",
-  ],
-  [
-    "--salt-status-error-background-high",
-    "--salt-status-error-background-emphasize",
-  ],
-
-  ["--salt-text-primary-color", "--salt-text-primary-foreground"],
-  ["--salt-text-secondary-color", "--salt-text-secondary-foreground"],
-
-  ["--salt-spacing-unit", "--salt-size-unit"],
-
-  ["--salt-color-grey-10", "--salt-color-gray-10"],
-  ["--salt-color-grey-20", "--salt-color-gray-20"],
-  ["--salt-color-grey-30", "--salt-color-gray-30"],
-  ["--salt-color-grey-40", "--salt-color-gray-40"],
-  ["--salt-color-grey-50", "--salt-color-gray-50"],
-  ["--salt-color-grey-60", "--salt-color-gray-60"],
-  ["--salt-color-grey-70", "--salt-color-gray-70"],
-  ["--salt-color-grey-80", "--salt-color-gray-80"],
-  ["--salt-color-grey-90", "--salt-color-gray-90"],
-  ["--salt-color-grey-100", "--salt-color-gray-100"],
-  ["--salt-color-grey-200", "--salt-color-gray-200"],
-  ["--salt-color-grey-300", "--salt-color-gray-300"],
-  ["--salt-color-grey-400", "--salt-color-gray-400"],
-  ["--salt-color-grey-500", "--salt-color-gray-500"],
-  ["--salt-color-grey-600", "--salt-color-gray-600"],
-  ["--salt-color-grey-700", "--salt-color-gray-700"],
-  ["--salt-color-grey-800", "--salt-color-gray-800"],
-  ["--salt-color-grey-900", "--salt-color-gray-900"],
-
-  ["--salt-zIndex-appheader", "--salt-zIndex-appHeader"],
-  ["--salt-zIndex-dragobject", "--salt-zIndex-dragObject"],
-  ["--salt-zIndex-contextmenu", "--salt-zIndex-contextMenu"],
-  ["--salt-zIndex-tooltip", "--salt-zIndex-flyover"],
-];
-
-const css130RenameMap = [
-  [
-    "--salt-differential-positive-foreground",
-    "--salt-status-positive-foreground",
-  ],
-  [
-    "--salt-differential-negative-foreground",
-    "--salt-status-negative-foreground",
-  ],
-];
-
 // <-------- TS Code ---------->
 
 if (mode === undefined || mode === "ts") {
-  console.log("Initialising TypeScript project from", tsconfig);
+  // Check, in case of monorepo which doesn't have tsconfig at the root
+  const tsConfigExisted = existsSync(tsconfig);
 
   const project = new Project({
     // Optionally specify compiler options, tsconfig.json, in-memory file system, and more here.
     // If you initialize with a tsconfig.json, then it will automatically populate the project
     // with the associated source files.
     // Read more: https://ts-morph.com/setup/
-    tsConfigFilePath: tsconfig,
+    tsConfigFilePath: tsConfigExisted ? tsconfig : undefined,
   });
 
   // console.log(project);
 
-  // project.addSourceFilesFromTsConfig();
+  if (tsConfigExisted) {
+    console.log("Initialising TypeScript project from", tsconfig);
+    // project.addSourceFilesFromTsConfig();
+  } else {
+    console.log(
+      "Initialising TypeScript project from source glob:",
+      tsSourceGlob
+    );
+    // Very bad way of handling monorepo, adding all ts files from `tsSourceGlob` args, by default is all ts* in packages folder
+    project.addSourceFilesAtPaths(tsSourceGlob);
+  }
 
   const sourceFiles = project.getSourceFiles();
   console.log("Found", sourceFiles.length, "source files");
@@ -266,6 +206,10 @@ if (mode === undefined || mode === "css") {
 
   if (gt(v130, fromVersion) && lte(v130, toVersion)) {
     cssMigrationMapArray.push(...css130RenameMap);
+  }
+
+  if (gt(v140, fromVersion) && lte(v140, toVersion)) {
+    cssMigrationMapArray.push(...css140RenameMap);
   }
 
   const cssMigrationMap = new Map(cssMigrationMapArray);
