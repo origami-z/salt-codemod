8521d4cd739093269cbef9bf7376ff52a53045f2
origami-z <davidcui2@me.com>
2023-05-12 14:16:25 +0100

    Update test

--- __tests__/migration/utils.spec.js
+++ __tests__/migration/utils.spec.js
@@ -2,12 +2,16 @@ import { getCssRenameCheckRegex } from "../../migration/utils.js";
 
 describe("getCssRenameCheckRegex", () => {
   test("regex match var intended", () => {
-    const actual = getCssRenameCheckRegex(new Map([["--salt-a", "--salt-b"]]));
+    const actual = getCssRenameCheckRegex(
+      new Map([["--salt-a", "--salt-a-new"]])
+    );
     expect(actual.test("--my-var: var(--salt-a);")).toBe(true);
     expect(actual.test("--my-var: var(--salt-b);")).toBe(false);
   });
   test("regex does not match var with other suffix", () => {
-    const actual = getCssRenameCheckRegex(new Map([["--salt-a", "--salt-b"]]));
+    const actual = getCssRenameCheckRegex(
+      new Map([["--salt-a", "--salt-a-new"]])
+    );
     expect(actual.test("--my-var: var(--salt-a-extra);")).toBe(false);
   });
 });
